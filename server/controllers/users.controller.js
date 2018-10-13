aaea16178262b252aea7a6c14213f3c1510143ba
mtaweela <mohamed.taweela56@gmail.com>
2018-10-13 14:52:56 +0200

    -- add the main controllers for the user managment

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,40 @@
+var { User } = require("./../models/user");
+
+
+let getMe = (req, res) => {
+  res.send()
+};
+
+
+let addUser = (req, res) => {
+  res.send()
+};
+
+
+let editUser = (req, res) => {
+  res.send()
+}; 
+
+
+let deleteUser = (req, res) => {
+  res.send()
+};
+
+
+let login = (req, res) => {
+  res.send()
+};
+
+
+let logout = (req, res) => {
+  res.send()
+};
+
+module.exports = {
+  getMe,
+  addUser,
+  editUser,
+  deleteUser,
+  login,
+  logout
+};
--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
-router
-    .get("/me", (req, res) => {res.send("me")})
-    .post("/", (req, res) => {res.send('root')})
-    .post("/login", (req, res) => {res.send('login')})
-    .delete('/me/token', (req, res) => {res.send('logout')})
+var usersController = require('../controllers/users.controller');
 
+router
+    .get("/me", usersController.getMe)
+    .post("/", usersController.addUser)
+    .put('/me', usersController.editUser)
+    .delete('/me', usersController.deleteUser)
+    .post("/login", usersController.login)
+    .delete('/me/token', usersController.logout)
 
 module.exports = router;
